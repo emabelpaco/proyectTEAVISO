1f46987b4a296fc91bd4d3066519010dde881a12
emabelpaco <45299597+emabelpaco@users.noreply.github.com>
2022-08-20 16:34:14 -0300

    info user primera parte

--- screens/account/UserLogged.js
+++ screens/account/UserLogged.js
@@ -1,24 +1,61 @@
-import React from "react";
+import React, { useState, useRef, useEffect } from "react";
 import { StyleSheet, Text, View } from "react-native";
 import { useNavigation } from "@react-navigation/native";
 import { Button } from "react-native-elements";
-import { closeSesion } from "../../utils/actions";
+import { closeSesion, getCurrentUser } from "../../utils/actions";
+import Toast from "react-native-easy-toast";
+import Loading from "../../components/Loading";
+import InfoUser from "./InfoUser";
 
 export default function UserLogged() {
+    const toastRef = useRef()
     const navigation = useNavigation()
 
+    const [loading, setLoading] = useState(false)
+    const [loadingText, setLoadingText] = useState("")
+    const [user, setUser] = useState(null)
+
+    useEffect(() => {
+        setUser(getCurrentUser())
+    }, [])
+
     return (
-        <View>
-            <Text>User Logged</Text>
+        <View style={styles.container}>
+            {
+                user && <InfoUser user={user}/>
+            }
+            <Text>Account options</Text>
             <Button
                 title="Cerra Sesión"
+                buttonStyle={styles.btnCloseSesion}
+                titleStyle={styles.btnCloseSesionText}
                 onPress={() => {
                     closeSesion()
                     navigation.navigate("mensaje")
                 }}
             />
+            <Toast ref={toastRef} position="center" opacity={0.9}/>
+            <Loading isVisible={loading} text={loadingText}/>
         </View>
     )
 }
 
-const styles = StyleSheet.create({})
+const styles = StyleSheet.create({
+    container: {
+        minHeight: "100%",
+        backgroundColor: "#f9f9f9"
+    },
+    btnCloseSesion: {
+        marginTop: 30,
+        borderRadius: 5,
+        backgroundColor:"#FFFFFF",
+        borderTopWidth: 1,
+        borderTopColor: "#57d5ae",
+        borderBottomWidth: 1,
+        borderBottomColor: "#57d5ae",
+        paddingVertical: 10
+    },
+    btnCloseSesionText: {
+        color: "#57d5ae"
+    }
+})
