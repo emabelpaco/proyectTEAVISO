bb1d4a815a59df6ffde0a6c0be333b774c93411a
emabelpaco <45299597+emabelpaco@users.noreply.github.com>
2022-08-21 19:59:14 -0300

    crear addMensajeForm

---
+++ components/mensajes/AddMensajeForm.js
@@ -0,0 +1,12 @@
+import React from "react";
+import { StyleSheet, Text, View } from "react-native";
+
+export default function AddMensajeForm({toasRef, setLoading}) {
+    return (
+        <View>
+            <Text>Add mensaje form</Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({})
--- screens/mensajes/AddMensaje.js
+++ screens/mensajes/AddMensaje.js
@@ -1,11 +1,26 @@
-import React from "react";
+import React, { useRef, useState } from "react";
 import { StyleSheet, Text, View } from "react-native";
+import AddMensajeForm from "../../components/mensajes/AddMensajeForm";
+import Toast from "react-native-easy-toast";
+import { KeyboardAwareScrollView } from "react-native-keyboard-aware-scroll-view";
+import Loading from "../../components/Loading";
 
-export default function AddMensaje() {
+export default function AddMensaje({ navigation }) {
+    const toastRef = useRef()
+    const [loading, setLoading] = useState(false)
+
     return (
-        <View>
-            <Text>add mensaje</Text>
-        </View>
+        <KeyboardAwareScrollView>
+            <AddMensajeForm
+                toastRef={toastRef}
+                setLoading={setLoading}
+            />
+            <Loading
+                isVisible={loading}
+                text="Creando mensaje..."
+            />
+            <Toast ref={toastRef} position="center" opacity={0.9}/>
+        </KeyboardAwareScrollView>
     )
 }
 
